e5c9d68a5cbe22c4e7915d8f845357f6d68e1434
Jujuzix <maronez.juliana@gmail.com>
2023-09-02 23:16:25 -0300

    Continuação

--- helloWord/src/main.jsx
+++ helloWord/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Todo from './Todo.jsx';
+
+
+import Home from './Home/Home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/todo' element ={<Todo />}></Route>
+       </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
